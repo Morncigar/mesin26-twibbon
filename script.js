80387ed887d8b074e0c7062e413919d3ec24670a
Morncigar <fadliapbkc@gmail.com>
2026-08-27 08:51:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
    NOW PLAYING — MESIN ’26 | Client-Side Image Compositor (4:5 Version)
    ================================================= */
 
-const FRAME_URL = "assets/frame-final.png";
+const FRAME_URL = "assets/twibbon.png";
 const EXPORT_WIDTH = 1080;
 const EXPORT_HEIGHT = 1350;
 
